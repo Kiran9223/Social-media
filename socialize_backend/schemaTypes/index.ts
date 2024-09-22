63be3a8892233874957afa6a8af9a58f6b82485a
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-09-22 12:19:59 -0700

    setup

---
+++ socialize_backend/schemaTypes/index.ts
@@ -0,0 +1,17 @@
+export const schemaTypes = [
+    {
+        name: 'user',
+        fields: [
+            {
+                name: 'name',
+                type: 'string',
+                required: true
+            },
+            {
+                name: 'image',
+                type: 'string',
+                required: true
+            },
+        ]
+    }
+]
